b4810910e87e0ad7be2bef6e7b54beb8684fd7b1
Dr_McFish <61105022+Dr-McFish@users.noreply.github.com>
2021-04-18 10:51:57 +0200

    Add 'debug' and 'release' rules to makefile

---
+++ build/include/POP/POP.h
@@ -0,0 +1,26 @@
+#ifndef POP_H
+# define POP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void POP_opts_init();
+void POP_opts_cleanup();
+
+int		POP_get_argc();
+char**	POP_get_argv();
+
+/* NULL and '\\0' are to be used if the corresponding name is not desired */
+void POP_new_bool_opt(bool* output_target, 	char short_name, char* long_name);
+/* NULL and '\\0' are to be used if the corresponding name is not desired */
+void POP_new_str_opt(char** output_target,	char short_name, char* long_name);
+/* NULL and '\\0' are to be used if the corresponding name is not desired */
+void POP_new_int_opt(int* output_target,	char short_name, char* long_name);
+
+enum POPparce_return_code{	PARCE_SUCCSEES, PARCE_FAIL, PARCE_FAIL_INVALID_OPT,
+							PARCE_FAIL_MISSING_ARG, PARCE_FAIL_NOT_A_NUMBER,
+							PARCE_FAIL_DUPLICATE_OPTION};
+enum POPparce_return_code POP_parce(int argc, char** argv);
+
+
+#endif
